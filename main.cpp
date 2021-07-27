29c176a772653cdf46a46f07468d569f0b4f8779
Kilbane <kilbanen@tcd.ie>
2021-07-27 22:11:00 +0100

    Added comparison steps to the visualisation, which really shows how slow selection sort is

--- main.cpp
+++ main.cpp
@@ -13,8 +13,16 @@ void selectionSort(Bar *bars, int size) {
   for(int i = 0; i < size; i++) {
     int min = i;
     for(int j = i + 1; j < size; j++) {
-      if(bars[j].height < bars[min].height)
+      bars[min].draw(40, min, 1);
+      bars[j].draw(40, j, 3);
+      getch();
+      bars[j].draw(40, j, 1);
+      bars[min].draw(40, min, 3);
+      getch();
+      if(bars[j].height < bars[min].height) {
+        bars[min].draw(40, min, 1);
         min = j;
+      }
     }
     if(min != i) {
       swap(bars, min, i);
@@ -28,10 +36,12 @@ void selectionSort(Bar *bars, int size) {
 int main() {
   initscr();
   curs_set(0);
+  noecho();
   start_color();
 
   init_pair(1, COLOR_BLACK, COLOR_WHITE);
   init_pair(2, COLOR_BLACK, COLOR_MAGENTA);
+  init_pair(3, COLOR_BLACK, COLOR_RED);
 
   srand(time(0));
 
