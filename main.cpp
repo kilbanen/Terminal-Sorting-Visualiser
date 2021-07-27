be287b1c6efce4a82cec76173b3d50ce2601a89c
Kilbane <kilbanen@tcd.ie>
2021-07-27 18:48:15 +0100

    Randomised bar heights

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <ncurses.h>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 #include "bar.h"
 
 int main() {
   initscr();
   curs_set(0);
 
+  srand(time(0));
+
   std::vector<Bar> bars;
 
   for(int i = 0; i < 40; i++) {
-    bars.push_back(i);
+    bars.push_back(rand() % 40);
     bars[i].draw(40, i);
   }
 
